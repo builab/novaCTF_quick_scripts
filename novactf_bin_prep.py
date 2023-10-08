246c70b319182bd6711b718a22dee5bee8ec06a0
builab <huy.bui@mcgill.ca>
2023-10-08 07:18:09 -0400

    Update novactf_bin_prep.py

--- novactf_bin_prep.py
+++ novactf_bin_prep.py
@@ -10,7 +10,7 @@
 import sys
 import os
 import shutil
-import multiprocessing
+import subprocess, multiprocessing
 
 def print_usage():
 	print("python novactf_bin_prep.py tomolist binfactor no_proc")
